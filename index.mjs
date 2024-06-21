6496b9a5f9db23b1f99d48a93f13c090def95c4a
Francois Daoust <fd@tidoust.net>
2024-06-21 18:01:55 +0200

    Add comments about cases worth analyzing

--- index.mjs
+++ index.mjs
@@ -1,6 +1,53 @@
 import webref from '../../w3c/webref/ed/index.json' with { type: 'json' };
 import reffy from 'reffy';
 
+const specsThatUseInParallelWithoutLink = [
+  'encrypted-media',
+  'webcodecs',
+  'webrtc'
+];
+
+async function analyze(spec) {
+  // Things worth analyzing:
+  // - Absence of any explicit algorithm when IDL is present, looking for `class="algorithm"`.
+  // - Presence of implicit algorithms that could be explicitly flagged, looking for lists that look like steps.
+  // - Use of "in parallel" in an algorithm without linking back to HTML
+  // - Use of a "queue a task" variant without linking back to HTML
+  // - Use of "enqueue the following steps" without linking back to HTML
+  // - Algorithm that is called from within "in parallel" steps of another algorithm and does not start with an "Assert" clause
+  // - Use of "in parallel" followed by "resolve/reject" without any "queue a task", see cases below
+  // - Use of "in parallel" followed by "fire an event" without any "queue a task", see cases below
+
+  // Case: "Assert: these steps are running in parallel."
+  // https://fedidcg.github.io/FedCM/#attempt-to-disconnect
+
+  // Case: "Run these steps in parallel" with nesting
+  // https://privacycg.github.io/requestStorageAccessFor/#dom-document-requeststorageaccessfor
+  // https://w3c.github.io/webdriver-bidi/#commands-sessionend
+
+  // Case: "run the remaining steps in parallel" without nesting
+  // https://fullscreen.spec.whatwg.org/#dom-element-requestfullscreen
+
+  // Case: inline "in parallel"
+  // https://w3c.github.io/webcodecs/#dom-imagedecoder-istypesupported
+
+  // Note: "Queue a task" may be used with nesting:
+  // https://webassembly.github.io/content-security-policy/js-api/#asynchronously-compile-a-webassembly-module
+  // https://privacycg.github.io/storage-access/#dom-document-requeststorageaccess
+
+  // Note: "Queue a task" may also be used inline:
+  // https://wicg.github.io/WebApiDevice/device_attributes/#dom-navigatormanageddata-getannotatedassetids
+  // https://w3c.github.io/deviceorientation/#dom-deviceorientationevent-requestpermission
+  // https://webmachinelearning.github.io/webnn/#dom-ml-createcontext
+
+  // Note: No easy way to detect that an algorithm is called "in parallel"
+  // unless it starts with an "Assert" clause
+
+  // Note: It's going to be hard to process specs that define their own queue
+  // and "queue a task" mechanism
+  // https://wicg.github.io/background-fetch/#queue-a-bgfetch-task
+}
+
 async function main() {
   const crawl = await reffy.expandCrawlResult(webref, '../../w3c/webref/ed') ;
 
@@ -11,7 +58,8 @@ async function main() {
   const singlepageUrl = multipageUrl.replace(/\/multipage\/[^#]+#/, '\/#');
 
   // Extract all specs that link to the definition
-  const specsWithInParallel = crawl.results.filter(spec =>
+  const specs = crawl.results.filter(spec =>
+    specsThatUseInParallelWithoutLink.includes(spec.shortname) ||
     Object.keys(spec.links?.rawlinks ?? {})
       .concat(Object.keys(spec.links?.autolinks ?? {}))
       .find(link =>
@@ -19,7 +67,8 @@ async function main() {
         (spec.links?.rawlinks?.[link] ?? spec.links?.autolinks?.[link]).anchors?.find(anchor => anchor === fragment)
       )
   );
-  console.log(specsWithInParallel
+
+  console.log(specs
     .map(spec => `- [ ] [${spec.title}](${spec.nightly?.url ?? spec.url})`)
     .join('\n'));
 }
